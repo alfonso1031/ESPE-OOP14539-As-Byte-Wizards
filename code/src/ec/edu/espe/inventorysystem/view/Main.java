65e16a149ec3ae95222e4d76325ade65be9634d0
Alfonso Alejandro Arroyo Paredes <arroyoalfonso1031@gmail.com>
2024-06-03 20:52:13 -0500

    First Files

---
+++ code/src/ec/edu/espe/inventorysystem/view/Main.java
@@ -0,0 +1,17 @@
+
+package ec.edu.espe.inventorysystem.view;
+
+/**
+ *
+ * @author Alfonso Arroyo <As-Byte Wizards> DDCC ESPE
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
