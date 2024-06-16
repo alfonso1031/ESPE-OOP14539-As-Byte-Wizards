d1b397272659de75981d8354484ff2e2c8253197
Kleiner Alexis Armas Gomez <kleinerarmas70@gmail.com>
2024-06-16 01:37:36 -0500

    Update Customer.java

--- CODE/Inventorysystem/src/main/java/ec/edu/espe/inventorysystem/model/Customer.java
+++ CODE/Inventorysystem/src/main/java/ec/edu/espe/inventorysystem/model/Customer.java
@@ -6,7 +6,7 @@ public class Customer {
     
     private String id;
     private String name;
-    private String address;
+    private String address ;
     private String email;
     private String phone;
     private String purchaseHistory;
